01c55105f5df69d1e6b9e388f0250b466893a41a
Ian livingstone <ian@akcdesigns.co.uk>
2020-12-05 12:16:05 +0000

    Setting out random message arrays and initial function

---
+++ script.js
@@ -0,0 +1,10 @@
+const messages = {
+    randomOne: ['You are ', 'You will '],
+    randomTwo: ['great!', 'beutiful!', ],
+    randomThree: ['be great!', 'be lucky today', ]
+};
+
+const randomMessageGenerator = (msgOne, msgTwo) => {
+    
+} 
+
